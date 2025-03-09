ca043ab4342abd5a9eb226edf0d4659bfb2de9ea
Goncalo Rui Antunes Nascimento Ferreira Leite <grui-ant@c1r1s1.42porto.com>
2025-03-09 12:56:34 +0000

    added Rush00

---
+++ Rush00/ex00/rush00.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grui-ant <grui-ant@student.42port...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 10:39:30 by grui-ant          #+#    #+#             */
+/*   Updated: 2025/02/22 17:44:34 by grui-ant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	print_corner(int i, int j, int x, int y);
+
+void	print_border(int i, int j, int x, int y);
+
+void	print_corner(int i, int j, int x, int y)
+{
+	if (i == 0 && j == 0)
+		ft_putchar('o');
+	else if (i == x - 1 && j == 0)
+	{
+		ft_putchar('o');
+	}
+	else if (i == 0 && j == y - 1)
+	{
+		ft_putchar('o');
+	}
+	else if (i == x - 1 && j == y - 1)
+		ft_putchar('o');
+}
+
+void	print_border(int i, int j, int x, int y)
+{
+	if (j == 0 || j == y - 1)
+	{
+		ft_putchar('-');
+	}
+	else if (i == 0 || i == x - 1)
+	{
+		ft_putchar('|');
+	}
+	else
+	{
+		ft_putchar(' ');
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	j;
+	int	i;
+
+	j = 0;
+	while (j < y)
+	{
+		i = 0;
+		while (i < x)
+		{
+			if ((i == 0 || i == x - 1) && (j == 0 || j == y - 1))
+			{
+				print_corner(i, j, x, y);
+			}
+			else
+			{
+				print_border(i, j, x, y);
+			}
+			i++;
+		}
+		ft_putchar('\n');
+		j++;
+	}
+}
