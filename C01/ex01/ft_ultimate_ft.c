634abba7cbe63d20b1a49cd1d826e589bd9b6bfe
Goncalo Rui Antunes Nascimento Ferreira Leite <grui-ant@c1r1s1.42porto.com>
2025-03-09 12:46:26 +0000

    added C01

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: grui-ant <grui-ant@student.42port...>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 17:03:58 by grui-ant          #+#    #+#             */
+/*   Updated: 2025/02/25 14:38:52 by grui-ant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+//#include <stdio.h>
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*int	main(void)
+{
+	int	a;
+
+	a = 3;
+	printf ("%d\n", a);
+
+	int *ptr1 = &a; 
+	int **ptr2 = &ptr1;
+	int ***ptr3 = &ptr2;
+	int ****ptr4 = &ptr3;
+	int *****ptr5 = &ptr4;
+	int ******ptr6 = &ptr5;
+	int *******ptr7 = &ptr6;
+	int ********ptr8 = &ptr7;
+	int *********ptr9 = &ptr8;
+	ft_ultimate_ft(ptr9);
+	printf ("%d\n", a);
+}
+*/
